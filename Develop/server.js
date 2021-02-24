9b5ee27cfd106be83ca93a6496f437c0d529a26b
alexaergun <alexaergun@gmail.com>
2021-02-24 11:52:21 -0700

    fixed index.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const apiRoutes = require("./routes/api.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,9 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 app.use(apiRoutes);
